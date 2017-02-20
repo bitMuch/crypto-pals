a043dc1fd2db05bd1123aebedb6271cb4b85cb5c
bitMuch <bitMuch@protonmail.com>
2017-02-20 05:12:11 +0000

    [+] ch1.py

---
+++ ch1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python     #
+# Created by c@caine  #
+# On: 20/02/2017      #
+# --- Preamble -------+
+import base64                                   # to encode/decode b64
+from sys import argv                            # for commaad line arg
+# --- Definitions --- #
+s = argv[1]
+# --- Functions --- #
+
+def hex_to_base64(hex_in):                      # converts hex to a byte array
+  b64_out = base64.b64encode(bytearray.fromhex(hex_in))     
+                                                # then the byte array to b64
+  return (b64_out)                              # spits base back
+
+def main ():
+  hex_to_base64(s)                              # call the function
+
+# --- Main --- #
+if __name__ == "__main__":
+  main()
